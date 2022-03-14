6962f4378edd4ccc2d6045782ba3f2fd48e6f616
Laustrup <laust.bonnesen@mail.com>
2022-03-14 13:00:09 +0100

    First written js

---
+++ static/javascripts/render_mains.js
@@ -0,0 +1,13 @@
+function renderAboutMe() {
+    document.getElementById("main_content").innerHTML = ``;
+    document.getElementById("main_content").innerHTML = `
+    <p>This is about me</p>
+    `;
+}
+
+function renderExperience() {
+    document.getElementById("main_content").innerHTML = ``;
+    document.getElementById("main_content").innerHTML = `
+    <p>This is my experiences</p>
+    `;
+}
